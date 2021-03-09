74a6f0a6ec8aa42391d0fd08c9d16c374d3b2039
Kole Gasior <jkole822@gmail.com>
2021-03-09 18:01:55 -0500

    create landing and portfolio comp and import into App comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./components/landing";
+import Portfolio from "./components/portfolio";
 
 const App = () => {
 	return (
-		<Switch>
-			<Route path="/" component={Landing} exact />
-			<Route path="/portfolio" component={Portfolio} exact />
-		</Switch>
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" component={Landing} exact />
+				<Route path="/portfolio" component={Portfolio} exact />
+			</Switch>
+		</BrowserRouter>
 	);
 };
 
