086540313fb84c5e64d95977a7fd746f47560000
Kole Gasior <jkole822@gmail.com>
2021-03-11 16:02:47 -0500

    change li to Typography comp

--- client/src/components/Skills/index.js
+++ client/src/components/Skills/index.js
@@ -22,22 +22,18 @@ const Skills = () => {
 				</Typography>
 				<Grid container spacing={3}>
 					<Grid item xs={12} md={6}>
-						<ul>
-							<li>HTML</li>
-							<li>CSS</li>
-							<li>JavaScript</li>
-							<li>jQuery</li>
-						</ul>
+						<Typography variant="body1">HTML</Typography>
+						<Typography variant="body1">CSS</Typography>
+						<Typography variant="body1">JavaScript</Typography>
+						<Typography variant="body1">jQuery</Typography>
 					</Grid>
 
 					<Grid item xs={12} md={6}>
-						<ul>
-							<li>React</li>
-							<li>React Hooks &amps; Context</li>
-							<li>Redux</li>
-							<li>Jest</li>
-							<li>Enzyme</li>
-						</ul>
+						<Typography variant="body1">React</Typography>
+						<Typography variant="body1">React Hooks &amps; Context</Typography>
+						<Typography variant="body1">Redux</Typography>
+						<Typography variant="body1">Jest</Typography>
+						<Typography variant="body1">Enzyme</Typography>
 					</Grid>
 				</Grid>
 			</div>
@@ -48,24 +44,20 @@ const Skills = () => {
 				</Typography>
 				<Grid container spacing={3}>
 					<Grid item xs={12} md={6}>
-						<ul>
-							<li>Node</li>
-							<li>Express</li>
-							<li>Template Engines</li>
-							<li>MongoDB</li>
-							<li>Mongoose</li>
-						</ul>
+						<Typography variant="body1">Node</Typography>
+						<Typography variant="body1">Express</Typography>
+						<Typography variant="body1">Template Engines</Typography>
+						<Typography variant="body1">MongoDB</Typography>
+						<Typography variant="body1">Mongoose</Typography>
 					</Grid>
 
 					<Grid item xs={12} md={6}>
-						<ul>
-							<li>JavaScript</li>
-							<li>Passport</li>
-							<li>SQL</li>
-							<li>mySQL</li>
-							<li>Sequelize</li>
-							<li>Jest</li>
-						</ul>
+						<Typography variant="body1">JavaScript</Typography>
+						<Typography variant="body1">Passport</Typography>
+						<Typography variant="body1">SQL</Typography>
+						<Typography variant="body1">mySQL</Typography>
+						<Typography variant="body1">SequeTypographyze</Typography>
+						<Typography variant="body1">Jest</Typography>
 					</Grid>
 				</Grid>
 			</div>
