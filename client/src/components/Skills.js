83b48ccac662dad670cb8fbba18b16cff09a4f43
Kole Gasior <jkole822@gmail.com>
2021-03-12 17:20:05 -0500

    add responsive margins

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -4,6 +4,8 @@ import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 
 import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
 
 const Skills = () => {
 	const useStyles = makeStyles({
@@ -17,94 +19,101 @@ const Skills = () => {
 	});
 
 	const classes = useStyles();
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down("sm"));
 
 	return (
-		<>
-			<Box mb={6}>
-				<Typography variant="h3" className={classes.header} align="center">
-					Front End Skills
-				</Typography>
-				<Grid container alignItems="center" justify="center">
-					<Grid item xs={4}>
-						<Typography align="center" variant="body1">
-							HTML
+		<Box my={13}>
+			<Grid container spacing={5}>
+				<Grid item xs={12} md={6}>
+					<Box mb={matches ? 5 : 0}>
+						<Typography variant="h3" className={classes.header} align="center">
+							Front End Skills
 						</Typography>
-						<Typography align="center" variant="body1">
-							CSS
-						</Typography>
-						<Typography align="center" variant="body1">
-							JavaScript
-						</Typography>
-						<Typography align="center" variant="body1">
-							jQuery
-						</Typography>
-					</Grid>
+						<Grid container alignItems="center" justify="center">
+							<Grid item xs={4}>
+								<Typography align="center" variant="body1">
+									HTML
+								</Typography>
+								<Typography align="center" variant="body1">
+									CSS
+								</Typography>
+								<Typography align="center" variant="body1">
+									JavaScript
+								</Typography>
+								<Typography align="center" variant="body1">
+									jQuery
+								</Typography>
+							</Grid>
 
-					<Grid item xs={4}>
-						<Typography align="center" variant="body1">
-							React
-						</Typography>
-						<Typography align="center" variant="body1">
-							React Hooks & Context
-						</Typography>
-						<Typography align="center" variant="body1">
-							Redux
-						</Typography>
-						<Typography align="center" variant="body1">
-							Jest
-						</Typography>
-						<Typography align="center" variant="body1">
-							Enzyme
-						</Typography>
+							<Grid item xs={4}>
+								<Typography align="center" variant="body1">
+									React
+								</Typography>
+								<Typography align="center" variant="body1">
+									React Hooks & Context
+								</Typography>
+								<Typography align="center" variant="body1">
+									Redux
+								</Typography>
+								<Typography align="center" variant="body1">
+									Jest
+								</Typography>
+								<Typography align="center" variant="body1">
+									Enzyme
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+				</Grid>
+
+				<Grid item xs={12} md={6}>
+					<Typography variant="h3" className={classes.header} align="center">
+						Back End Skills
+					</Typography>
+					<Grid container alignItems="center" justify="center">
+						<Grid item xs={4}>
+							<Typography align="center" variant="body1">
+								Node
+							</Typography>
+							<Typography align="center" variant="body1">
+								Express
+							</Typography>
+							<Typography align="center" variant="body1">
+								Template Engines
+							</Typography>
+							<Typography align="center" variant="body1">
+								MongoDB
+							</Typography>
+							<Typography align="center" variant="body1">
+								Mongoose
+							</Typography>
+						</Grid>
+
+						<Grid item xs={4}>
+							<Typography align="center" variant="body1">
+								JavaScript
+							</Typography>
+							<Typography align="center" variant="body1">
+								Passport
+							</Typography>
+							<Typography align="center" variant="body1">
+								SQL
+							</Typography>
+							<Typography align="center" variant="body1">
+								mySQL
+							</Typography>
+							<Typography align="center" variant="body1">
+								Sequelize
+							</Typography>
+							<Typography align="center" variant="body1">
+								Jest
+							</Typography>
+						</Grid>
 					</Grid>
 				</Grid>
-			</Box>
-			<Box mb={10}>
-				<Typography variant="h3" className={classes.header} align="center">
-					Back End Skills
-				</Typography>
-				<Grid container alignItems="center" justify="center">
-					<Grid item xs={4}>
-						<Typography align="center" variant="body1">
-							Node
-						</Typography>
-						<Typography align="center" variant="body1">
-							Express
-						</Typography>
-						<Typography align="center" variant="body1">
-							Template Engines
-						</Typography>
-						<Typography align="center" variant="body1">
-							MongoDB
-						</Typography>
-						<Typography align="center" variant="body1">
-							Mongoose
-						</Typography>
-					</Grid>
-
-					<Grid item xs={4}>
-						<Typography align="center" variant="body1">
-							JavaScript
-						</Typography>
-						<Typography align="center" variant="body1">
-							Passport
-						</Typography>
-						<Typography align="center" variant="body1">
-							SQL
-						</Typography>
-						<Typography align="center" variant="body1">
-							mySQL
-						</Typography>
-						<Typography align="center" variant="body1">
-							Sequelize
-						</Typography>
-						<Typography align="center" variant="body1">
-							Jest
-						</Typography>
-					</Grid>
-				</Grid>
-			</Box>
-		</>
+			</Grid>
+		</Box>
 	);
 };
 
